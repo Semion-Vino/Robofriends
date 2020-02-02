8e0c60f9bee15cc90ccc7d2a038e6009b3344e99
sami <samistygian@gmail.com>
2020-02-02 17:04:28 +0200

    Updates

---
+++ precache-manifest.626d4be7007058c2b1a840bb164bc3b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "770ae518852ff00e21156ba5ed50893e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8f02dcb18e22329755aa",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "01370f553c7613f5a226",
+    "url": "/robofriends/static/css/main.221559ec.chunk.css"
+  },
+  {
+    "revision": "8f02dcb18e22329755aa",
+    "url": "/robofriends/static/js/2.7eccd650.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robofriends/static/js/2.7eccd650.chunk.js.LICENSE"
+  },
+  {
+    "revision": "01370f553c7613f5a226",
+    "url": "/robofriends/static/js/main.96f69477.chunk.js"
+  },
+  {
+    "revision": "2d3fcd53f2c682e07d19",
+    "url": "/robofriends/static/js/runtime-main.aab978f1.js"
+  }
+]);
